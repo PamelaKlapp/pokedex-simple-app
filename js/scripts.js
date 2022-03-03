a0797701018b17db624a74835a5e93f3710e3f4d
PamelaKlapp <84852088+PamelaKlapp@users.noreply.github.com>
2022-03-03 11:54:07 +0100

    deleted previous script & create pokemonList
    pokemonList within are 4 different pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello World!');
-let favoriteFood="Pasta";
-document.write(favoriteFood);
+let pokemonList= [
+  {name:'Pidgeotto', height: 1.1 , type:["flying", "normal"]},
+  {name:'Persian', height: 1 , type:"normal"},
+  {name:'Jynx', height: 1.4 , type:["psychic", "ice"]},
+  {name:'Umbreon', height: 1 , type:"dark"},
+];
