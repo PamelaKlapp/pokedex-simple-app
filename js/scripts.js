99e35cde7cd42dc059ded8fc810a8debe70ab4b5
PamelaKlapp <pameklapp@gmail.com>
2022-03-03 19:57:24 +0100

    improved readability

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
-let pokemonList= [
-  {name: 'Pidgeotto' , height: 1.1 , typeList: ["flying", "normal"] },
-  {name: 'Persian' , height: 1 , typeList: ["normal"] },
-  {name: 'Jynx' , height: 1.4 , typeList: ["psychic", "ice"] },
-  {name: 'Umbreon' , height: 1 , typeList: ["dark"] },
+let pokemonList = [
+  { name: "Pidgeotto", height: 1.1, typeList: ["flying", "normal"] },
+  { name: "Persian", height: 1, typeList: ["normal"] },
+  { name: "Jynx", height: 1.4, typeList: ["psychic", "ice"] },
+  { name: "Umbreon", height: 1, typeList: ["dark"] },
 ];
 
-for (let i= 0; i < pokemonList.length; i++){
-  if(pokemonList[i].height > 1.2){
-  document.write('<p class="biggest_pokemon">' + pokemonList[i].name + " (height: " + pokemonList[i].height + ") " + " THIS IS THE BIGGEST ONE " +'</p>');
-} else { '<p class="pokemon">' + document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") "+ '</p>');
-};
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 1.2) {
+    document.write(
+      '<p class="biggest_pokemon">' +
+        pokemonList[i].name +
+        " (height: " +
+        pokemonList[i].height +
+        ") " +
+        " THIS IS THE BIGGEST ONE " +
+        "</p>"
+    );
+  } else {
+    '<p class="pokemon">' +
+      document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") " + "</p>");
+  }
 }
-
